dc447de569f22339d1d945f4cf533be2f559c7f1
Gustavo Marmentini <gustavomarini9@gmail.com>
2018-11-17 02:23:55 -0200

    Reset changes

--- test_client.py
+++ test_client.py
@@ -1,9 +1,8 @@
 from libs.RPCClient import RPCClient
 
-for i in range(1, 1000):
-    client = RPCClient()
-    client.connect()
-    
-    function_call = client.execute("is_even", 569)
-    print(function_call)
+client = RPCClient()
+client.connect()
+
+function_call = client.execute("is_even", 569)
+print(function_call)
 
